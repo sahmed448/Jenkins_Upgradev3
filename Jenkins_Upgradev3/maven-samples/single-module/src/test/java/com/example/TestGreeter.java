56eb366b88b92f0ff7e377b8b1a2b1e816e98c50
sahmed448 <javed7504@gmail.com>
2022-12-25 12:18:13 -0500

    modifing Test greet

--- Jenkins_Upgradev3/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ Jenkins_Upgradev3/maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -42,7 +42,7 @@ public class TestGreeter {
   }
 
     @Test
-  public void greetShouldIncludeGreetingMessage() {
+  public void greetShouldIncludeGreetingMessageFromJaved() {
     String someone = "Hello World, Thanks From Javed";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
